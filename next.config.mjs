907fbe58bb21a4b38a27070f3b92e8b563c57547
Zizwe Mtonga <zizwemtonga@Zizwes-MacBook-Air.local>
2026-07-20 08:17:17 -0700

    add basePath for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-
+  basePath: isProd ? "/zizwa_consult" : "",
+  assetPrefix: isProd ? "/zizwa_consult/" : "",
   images: {
     unoptimized: true,
   },
